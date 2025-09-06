610aab8704594c854e33323dc56f2337db562881
Baltej Singh <baltej963@gmail.com>
2025-09-06 16:07:45 +0530

    Optimize for Vercel deployment

--- next.config.js
+++ next.config.js
@@ -1,10 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-//  output: 'export',
   eslint: {
     ignoreDuringBuilds: true,
   },
-  images: { unoptimized: true },
+  images: { 
+    unoptimized: true,
+    domains: ['images.pexels.com'] // If you add any external images later
+  },
+  // Optimize for Vercel
+  experimental: {
+    serverComponentsExternalPackages: ['mongoose'],
+  },
 };
 
 module.exports = nextConfig;
